6f0ea7b9018a732e8143f8d311208b6fc46d85e7
Pride <20098992+PrideInt@users.noreply.github.com>
2024-06-19 14:28:11 -0400

    isolate k-mers

--- DeBruijn.cs
+++ DeBruijn.cs
@@ -5,6 +5,7 @@ namespace DeBruijn {
         static void Main(string[] args) {
             // Test run
 
+            /*
             Graph graph = new(5);
             graph.AddNode(1);
             graph.AddNode(2);
@@ -15,8 +16,36 @@ namespace DeBruijn {
             graph.vertices[0].Display();
             graph.vertices[1].Display();
             graph.edges[0].Display();
+            */
 
+            Console.WriteLine("Enter k: ");
+            string k = Console.ReadLine();
+            string[] kmers = IsolateKMers(k, 3);
+
+            foreach (string kmer in kmers) {
+                Console.WriteLine(kmer);
+            }
             Console.ReadLine();
+        }
+
+        static string[] IsolateKMers(string k, int length) {
+            string[] kmers = new string[k.Length];
+            for (int i = 0; i < kmers.Length; i++) {
+                var builder = new System.Text.StringBuilder();
+
+                if (i + length > k.Length) {
+                    builder.Append(k.Substring(i, k.Length - i));
+                    builder.Append(k.Substring(0, (i + length) - k.Length));
+                } else {
+                    builder.Append(k.Substring(i, length));
+                }
+                kmers[i] = builder.ToString();
+            }
+            return kmers;
+        }
+
+        static Graph GenerateGraph() {
+            return null;
         }
     }
 
@@ -58,7 +87,6 @@ namespace DeBruijn {
             edges[e++] = edge;
         }
     }
-
     class Node {
         public Node next, prev;
         public int data;
